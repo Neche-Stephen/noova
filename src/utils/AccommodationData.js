b5bd8af9ea69c8104fd4a527fe8ed5bb35325e76
Chinecherem <chinecheremubawike@gmail.com>
2023-12-09 14:30:28 +0100

    New changes

---
+++ src/utils/AccommodationData.js
@@ -0,0 +1,22 @@
+// AccommodationData.js
+const accommodationData = [
+    {
+      id: 1,
+      name: 'Student House 1',
+      location: 'City A',
+      capacity: 5,
+    },
+    {
+      id: 2,
+      name: 'Student House 2',
+      location: 'City B',
+      capacity: 8,
+    },
+    // ... add more accommodation objects as needed
+  ];
+  
+  export default accommodationData;
+//Name
+//Price
+//Location
+//  
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBATY0qTb7rSu-PZpzQtgsc-5gr9Lvvj_o",
+  authDomain: "noova-3e5b5.firebaseapp.com",
+  projectId: "noova-3e5b5",
+  storageBucket: "noova-3e5b5.appspot.com",
+  messagingSenderId: "94462454667",
+  appId: "1:94462454667:web:b5fe96d2846722f0531d8e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
